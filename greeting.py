6d41ca6b95ee716071a6b687e6bce8d109e2834d
Khushi Goud <khushigoud09@gmail.com>
2026-09-09 11:23:05 +0530

    Delete greeting.py

--- greeting.py
+++
@@ -1,7 +0,0 @@
-# This program takes name, age and city as input and displays a greeting.
-
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-city = input("Enter your city: ")
-
-print(f"My name is {name}, I am {age} years old and I live in {city}.")
