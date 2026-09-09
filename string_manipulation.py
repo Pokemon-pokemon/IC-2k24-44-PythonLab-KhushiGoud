b4065047d11edd9d7141ea08804f178176af14ac
Khushi Goud <khushigoud09@gmail.com>
2026-09-09 11:23:27 +0530

    Delete string_manipulation.py

--- string_manipulation.py
+++
@@ -1,8 +0,0 @@
-# This program performs different operations on a full name.
-
-name = input("Enter your full name: ")
-
-print("Uppercase:", name.upper())
-print("Lowercase:", name.lower())
-print("Reversed:", name[::-1])
-print("Length:", len(name))
