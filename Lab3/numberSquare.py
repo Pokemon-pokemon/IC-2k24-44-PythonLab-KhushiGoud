1ba6e8219579736c4d849d306cf4959cd5be0c84
Khushi Goud <khushigoud09@gmail.com>
2026-09-11 09:19:03 +0530

    Delete Lab3 directory

--- Lab3/numberSquare.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter the number: "))
-
-for i in range(2 * n - 1):
-    for j in range(2 * n - 1):
-        x = max(abs(n - 1 - i), abs(n - 1 - j))
-        print(x + 1, end=" ")
-    print()
